8186686790996a62f60156a0f8ccad2333a024eb
Mahmoud <devmahmoud.me@gmail.com>
2025-09-15 17:18:04 +0300

    Update Prisma dependencies and configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma";
+import { prisma } from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
 import { sendEmail } from "@/lib/mail";
 
 import { ResetPasswordEmail } from "@/emails/ResetPasswordEmail";
 import { VerificationEmail } from "@/emails/VerificationEmail";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
